530b90d0546602006c278f8683d0c01820aabeb0
Cosmin Memetea <cosmin.memetea@gmail.com>
2025-05-21 22:35:18 +0300

    init: #1 - Project Setup

---
+++ src/main/java/trainix/JadinApplication.java
@@ -0,0 +1,13 @@
+package trainix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JadinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/trainix/JadinApplicationTests.java
@@ -0,0 +1,13 @@
+package trainix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
